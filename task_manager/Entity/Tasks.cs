4db825d99a78e70ee9cf53007cea633860dabc91
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-07 01:00:01 -0300

    feat: add task entity

---
+++ task_manager/Entity/Tasks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task_manager.Repositories;
+
+namespace task_manager.Entity
+{
+    internal class Tasks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Tasks(int id, string title, string description) 
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+        }
+
+        public override string? ToString() => $"=================================\nId -> {Id}\nName -> {Title}\nDescription -> {Description}\n=================================";
+    }
+}
