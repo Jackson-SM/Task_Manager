07c3b238373888db255e15f5b1a97a1b1e8f387a
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-07 01:00:13 -0300

    feat: add task repository

---
+++ task_manager/Repositories/TaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task_manager.Entity;
+
+namespace task_manager.Repositories
+{
+    internal interface TaskRepository
+    {
+        public void Create(string title, string description);
+        public void ToListTasks();
+        public void Update(Tasks task);
+        public void Delete(int id);
+        public void GetById(int id);
+    }
+}
